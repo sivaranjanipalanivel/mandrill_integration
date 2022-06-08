a0f8c88bed30c35255bca46410b6bc865f0ec2ca
sivaranjanipalanivel <34833079+sivaranjanipalanivel@users.noreply.github.com>
2022-06-08 14:42:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os
+try: # for pip >= 10
+	from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+	from pip.req import parse_requirements
+import re, ast, os
 
-version = '0.0.1'
+# get version from __version__ variable in notification/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('notification/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
+
+#requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 setup(
-    name='mandrill_integration',
-    version=version,
-    description='Set communication status from Mandrill via webhooks',
-    author='Frappe Technologies Pvt. Ltd.',
-    author_email='team@frappe.io',
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=("frappe",),
+	name='mandrill_integration',
+	version=version,
+	description='Set communication status from Mandrill via webhooks',
+	author='Tridots Tech Private Ltd.',
+	author_email='info@valiantsystems.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+	# install_requires=[str(ir.req) for ir in requirements],
+	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
